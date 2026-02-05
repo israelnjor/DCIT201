c5e84836d0874377f75f9670079d1fe18f5e9d6d
Israelnjor <ibkiche@st.ug.edu.gh>
2026-02-05 18:52:38 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,26 @@
-public class Main {
+public class GradeReport {
 
-    public static void main(String[] args) {
-      
-    }
-    static void executeGradeReport(double score) {
+    public void executeGradeReport(double score) {
       if(!validateScore(score)) {
         System.out.println("Ivalid Score");
         return;
       }
       char grade = calculateLetterGrade(score);
-      displayPerfomanceMessage(grade);
+
+      displayPerformanceMessage(grade);
     }
 
     private boolean validateScore(double score) {
-        retrun score >= 0 && score <= 100;
+        return score >= 0 && score <= 100;
     }
 
     private char calculateLetterGrade(double score) {
-        if (score >= 90) return "A";
-        else if (score >= 80) return "B";
+        if (score >= 90) return 'A';
+        else if (score >= 80) return 'B';
         else if (score >= 70) return 'C';
         else if (score >= 60) return 'D';
         else if (score >= 50) return 'E';
-        else return "F";
+        else return 'F';
     }
 
     private void displayPerformanceMessage(char grade) {
@@ -34,10 +32,6 @@ public class Main {
             case 'F': System.out.println("Failing. Try harder!"); break;
             default: System.out.println("Unknown grade."); break;
         }
-    }
-    public static void main(String[] args {
-        GradeReport report = new GradeReport();
-        report.execueGradeReport(85);
     }
 }
     
