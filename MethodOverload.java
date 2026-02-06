05d39030d3dd77b490435b4169f62f404d65c925
Israelnjor <ibkiche@st.ug.edu.gh>
2026-02-06 00:05:46 +0000

    Add method overloads for calculating class average

--- MethodOverload.java
+++ MethodOverload.java
@@ -1,1 +1,22 @@
+public class ClassAverage {
+
+    public static double calculateClassAverage(double score1, double score2) {
+        return (score1 + score2) / 2;
+    }
+
+
+      public static double calculateClassAverage(double score1, double score2, double score3) {
+        return (score1 + score2 + score3) / 3;
+    }
+
+    public static double calculateClassAverage(double[] scores) {
+        double sum = 0;
+
+        for (double score : scores) {
+            sum += score;
+        }
+
+        return sum / scores.length;
+    }
+}
 
